680a5388ac600b3c8ff7f195d3f063ea75ba8572
Hail2DaCode <iovinob2@gmail.com>
2023-01-13 05:21:35 -0700

    NINJA BONUS!

--- BankAccount.py
+++ BankAccount.py
@@ -1,7 +1,9 @@
 class BankAccount:
+    all_accounts = []
     def __init__(self, int_rate, balance = 0,):
         self.balance = balance
         self.int_rate = int_rate
+        BankAccount.all_accounts.append(self)
     def deposit(self, amount, repeat = 1):
         self.balance += amount * repeat
         return self
@@ -18,6 +20,11 @@ class BankAccount:
         if self.balance > 0:
             self.balance += self.balance * self.int_rate
         return self
+    @classmethod
+    def info_for_all_accounts(cls):
+        for account in BankAccount.all_accounts:
+            account.display_account_info()
+
 
 account_1 = BankAccount(0.05, 1000)
 account_2 = BankAccount(0.08, 2000)
@@ -25,3 +32,4 @@ account_2 = BankAccount(0.08, 2000)
 # account_2.display_account_info()
 account_1.deposit(500).deposit(200).deposit(100).withdraw(500).yield_interest().display_account_info()
 account_2.deposit(250, 2).withdraw(200, 4).yield_interest().display_account_info()
+BankAccount.info_for_all_accounts()
